63360d0e6083a5ea64eca930db246c11fa660b39
ldcduc <ldcduc@apcs.vn>
2017-12-19 18:28:08 +0700

    Edit 3

--- Lab_4/cachelab-handout/csim.c
+++ Lab_4/cachelab-handout/csim.c
@@ -6,9 +6,6 @@
 #include <math.h>
 #include "cachelab.h"
 
-typedef unsigned long long int memoryAddress;
-
-
 struct cacheParameter {
     int s;
     int b;
@@ -23,7 +20,7 @@ struct cacheParameter {
 struct setLine {
     int latestUsed;
     int valid;
-    memoryAddress tag;
+    long long int tag;
     char *block;
 };
 
@@ -59,8 +56,7 @@ struct cache initCache (long long numberOfSets, int numberOfLines, long long siz
 }
 
 void cleanFunction(struct cache myCache, long long numberOfSets, int numberOfLines, long long sizeOfBlock) {
-    int indexOfSet;
-    for (indexOfSet = 0; indexOfSet < numberOfSets; indexOfSet++) {
+    for (int indexOfSet = 0; indexOfSet < numberOfSets; indexOfSet++) {
         struct cacheSet set = myCache.sets[indexOfSet];
         
         if (set.lines != NULL)
@@ -73,13 +69,12 @@ void cleanFunction(struct cache myCache, long long numberOfSets, int numberOfLin
 
 int detectEmptyLine(struct cacheSet exampleSet, struct cacheParameter exampleParameter) {
     int numberOfLines = exampleParameter.E;
-    int index;
     struct setLine line;
 
-    for (index = 0; index < numberOfLines; index++) {
-        line = exampleSet.lines[index];
+    for (int indexOfLine = 0; index < numberOfLines; index++) {
+        line = exampleSet.lines[indexOfLine];
         if (!line.valid)
-            return index;
+            return indexOfLine;
     }
     return -1;
 }
@@ -105,16 +100,16 @@ int detectEvictLine(struct cacheSet exampleSet, struct cacheParameter examplePar
     return minFreqUsage_index;
 }
 
-struct cacheParameter accessTheCacheData(struct cache myCache, struct cacheParameter exampleParameter, memoryAddress address) {
+struct cacheParameter accessTheCacheData(struct cache myCache, struct cacheParameter exampleParameter, long long int address) {
     int checkFullCache = 1;
 
     int numberOfLines = exampleParameter.E;
     int previousHit = exampleParameter.hits;
 
     int tagSize = (64 - exampleParameter.s - exampleParameter.b);
-    memoryAddress inputTag = address >> (exampleParameter.s + exampleParameter.b);
-    memoryAddress temp = address << tagSize;
-    memoryAddress indexOfSet = temp >> (tagSize + exampleParameter.b);
+    long long int inputTag = address >> (exampleParameter.s + exampleParameter.b);
+    long long int temp = address << tagSize;
+    long long int indexOfSet = temp >> (tagSize + exampleParameter.b);
 
     struct cacheSet exampleSet = myCache.sets[indexOfSet];
     
@@ -155,25 +150,22 @@ struct cacheParameter accessTheCacheData(struct cache myCache, struct cacheParam
     return exampleParameter;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	
-	struct cache myCache;
-	struct cacheParameter exampleParameter;
-	bzero(&exampleParameter, sizeof(exampleParameter));  // read the report for this function's purpose
-	long long numberOfSets;
-	long long sizeOfBlock;	
-	FILE *openTrace;
-	char instructionInTraceFile;
-	memoryAddress address;
-	int size;
-	char *trace_file;
-	char c;
-	/* this part takes in argument. More on how do I do this-> read report file */
-    while( (c=getopt(argc,argv,"s:E:b:t:vh")) != -1)
-	{
-        switch(c)
-		{
+    struct cache myCache;
+    struct cacheParameter exampleParameter;
+    bzero(&exampleParameter, sizeof(exampleParameter));  // read the report for this function's purpose
+    long long numberOfSets;
+    long long sizeOfBlock;	
+    FILE *openTrace;
+    char instructionInTraceFile;
+    long long int address;
+    int size;
+    char *trace_file;
+    char c;
+
+    while( (c=getopt(argc,argv,"s:E:b:t:vh")) != -1) {
+        switch(c) {
         case 's':
             exampleParameter.s = atoi(optarg);
             break;
@@ -192,39 +184,37 @@ int main(int argc, char **argv)
             exit(1);
         }
     }
-   /* end of take in arguments from command line */ 
 
- 	numberOfSets = pow(2.0, exampleParameter.s);   // get Number of set by 2^s
-	sizeOfBlock = pow(2.0, exampleParameter.b);  //  get sizeOfBlock by 2^b
-	exampleParameter.hits = 0;
-	exampleParameter.misses = 0;
-	exampleParameter.evicts = 0;
-	myCache = initCache (numberOfSets, exampleParameter.E, sizeOfBlock);
+    numberOfSets = pow(2.0, exampleParameter.s);   // get Number of set by 2^s
+    sizeOfBlock = pow(2.0, exampleParameter.b);  //  get sizeOfBlock by 2^b
+    exampleParameter.hits = 0;
+    exampleParameter.misses = 0;
+    exampleParameter.evicts = 0;
+    myCache = initCache (numberOfSets, exampleParameter.E, sizeOfBlock);
 
-	/* this part read file. More on how do I do this-> read report file */
-	openTrace  = fopen(trace_file, "r");
-	if (openTrace != NULL) {
-		while (fscanf(openTrace, " %c %llx,%d", &instructionInTraceFile, &address, &size) == 3) {
-			switch(instructionInTraceFile) {
-				case 'I':
-					break;
-				case 'L':
-					exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
-					break;
-				case 'S':
-					exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
-					break;
-				case 'M':
-					exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
-					exampleParameter = accessTheCacheData(myCache, exampleParameter, address);	
-					break;
-				default:
-					break;
-			}
-		}
-	}
-	/* end of read file */
+    openTrace  = fopen(trace_file, "r");
+    if (openTrace != NULL) {
+        while (fscanf(openTrace, " %c %llx,%d", &instructionInTraceFile, &address, &size) == 3) {
+            switch(instructionInTraceFile) {
+                case 'I':
+                    break;
+                case 'L':
+                    exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
+                    break;
+                case 'S':
+                    exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
+                    break;
+                case 'M':
+                    exampleParameter = accessTheCacheData(myCache, exampleParameter, address);
+                    exampleParameter = accessTheCacheData(myCache, exampleParameter, address);	
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 	
+
     printSummary(exampleParameter.hits, exampleParameter.misses, exampleParameter.evicts);
 	cleanFunction(myCache, numberOfSets, exampleParameter.E, sizeOfBlock);
 	fclose(openTrace);
